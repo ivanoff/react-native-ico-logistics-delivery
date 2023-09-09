c79175879fb626a646353cc3e88ec240db57ad38
Dimitry Ivanov <2@ivanoff.org.ua>
2023-09-09 22:45:17 +0300

    add types

--- typings.d.ts
+++ typings.d.ts
@@ -127,10 +127,35 @@ declare module 'react-native-ico-logistics-delivery' {
       'box-17' |
       'warehouse';
 
+    type backgroundType = 'circle' | 'rect' | 'button';
+
+    type positionType = 'top' | 'bottom' | 'right' | 'left' |
+      'top_right' | 'top_left' | 'bottom_right' | 'bottom_left';
+
+    type backgroundTypeObj = {
+      type?: backgroundType;
+      color?: string;
+      radius?: number;
+      borderRadius?: number;
+    };
+
+    type badgeTypeObj = {
+      value?: number | string;
+      fontSize?: string;
+      position?: positionType;
+      color?: string;
+      backgroundColor?: string;
+      radius?: number;
+    };
+
     interface IconProps {
       name: iconNames;
-      color: string;
-      size: number;
+      width?: number;
+      height?: number;
+      color?: string;
+      stroke?: number;
+      background?: backgroundType | backgroundTypeObj;
+      badge?: number | string | badgeTypeObj;
     }
 
     const Icon: (props: IconProps) => ReactNode;
@@ -138,4 +163,3 @@ declare module 'react-native-ico-logistics-delivery' {
     export { iconNames };
     export default Icon;
 }
-
